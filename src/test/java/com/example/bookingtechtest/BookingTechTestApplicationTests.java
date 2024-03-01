ce0487ae0d72960b069b9d81d99c299867c79357
Mateus Cesar <mateus.cesar@agnes.com.br>
2024-03-01 08:12:47 -0300

    feature : configuring database inside app properties

---
+++ src/test/java/com/example/bookingtechtest/BookingTechTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookingtechtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingTechTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
